9e280ec368bee47596c9285e5c5e0332edcb2c3f
mogana varshini <92911280+Mogana004@users.noreply.github.com>
2024-03-29 15:46:45 +0530

    Create code-7-seriesSum.py

---
+++ code-7-seriesSum.py
@@ -0,0 +1,12 @@
+
+python
+Copy code
+x = int(input("Enter the value of x: "))
+N = int(input("Enter the number of terms (N): "))
+total_sum = 0
+
+for i in range(1, 2*N, 2):
+    power = x ** i
+    total_sum += power
+
+print("The sum of the series is:", total_sum)
